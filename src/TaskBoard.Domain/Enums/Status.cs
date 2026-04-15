4bbc3d041f085b3db2cb68dfecb0ad236b56e3b2
André Pettersson <andre20030417@gmail.com>
2026-04-15 13:27:04 +0200

    Add domain entities and enums

---
+++ src/TaskBoard.Domain/Enums/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskBoard.Domain.Enums;
+
+public enum Status
+{
+    ToDo = 0,
+    InProgress = 1,
+    Done = 2
+}
---
+++ src/TaskBoard.Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using TaskBoard.Domain.Enums;
+
+namespace TaskBoard.Domain.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public required string UserName { get; set; }
+    public string PasswordHash { get; set; } = null!;
+    public Role Role { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    // Navigation property
+    public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+}
