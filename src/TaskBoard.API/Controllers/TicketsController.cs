d9861bc1d9073e5903fe6dc214af922c838207b2
André Pettersson <andre20030417@gmail.com>
2026-04-15 21:48:05 +0200

    Add create ticket endpoint with command and handler

---
+++ src/TaskBoard.API/Controllers/TicketsController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TaskBoard.Application.Commands.Tickets;
+
+namespace TaskBoard.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TicketsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public TicketsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    // POST api/tickets
+    [HttpPost]
+    public async Task<IActionResult> CreateTicket(CreateTicketCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return CreatedAtAction(nameof(CreateTicket), new { id = result }, result);
+    }
+}
