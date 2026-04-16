69e6bc72912cea1c1a85d002f469d905fcb74e12
André Pettersson <andre20030417@gmail.com>
2026-04-16 10:02:05 +0200

    Add get tickets endpoints with queries and handlers

---
+++ src/TaskBoard.Application/Queries/Tickets/GetAllTicketsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using TaskBoard.Application.Interfaces;
+using TaskBoard.Domain.Models;
+
+namespace TaskBoard.Application.Queries.Tickets;
+
+public class GetAllTicketsQueryHandler : IRequestHandler<GetAllTicketsQuery, IEnumerable<Ticket>>
+{
+    private readonly IRepository<Ticket> _repository;
+
+    public GetAllTicketsQueryHandler(IRepository<Ticket> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<Ticket>> Handle(GetAllTicketsQuery request, CancellationToken cancellationToken)
+    {
+        return await _repository.GetAllAsync();
+    }
+}
---
+++ src/TaskBoard.Application/Queries/Tickets/GetTicketByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using TaskBoard.Application.Interfaces;
+using TaskBoard.Domain.Models;
+
+namespace TaskBoard.Application.Queries.Tickets;
+
+public class GetTicketByIdQueryHandler : IRequestHandler<GetTicketByIdQuery, Ticket?>
+{
+    private readonly IRepository<Ticket> _repository;
+
+    public GetTicketByIdQueryHandler(IRepository<Ticket> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Ticket?> Handle(GetTicketByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _repository.GetByIdAsync(request.Id);
+    }
+}
